6f0b6038ce30b83978393c5381b1c46b8d7ead56
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-11 23:13:57 -0300

    implement get lists

---
+++ src/main/java/com/kebaudin/dslist/controllers/GameListController.java
@@ -0,0 +1,23 @@
+package com.kebaudin.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kebaudin.dslist.dto.GameListDTO;
+import com.kebaudin.dslist.services.GameListService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+  @Autowired
+  private GameListService gameListService;
+
+  @GetMapping
+  public List<GameListDTO> findAll() {
+    return gameListService.findAll();
+  }
+}
